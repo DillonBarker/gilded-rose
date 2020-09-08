a715633bcf24a8d801001e40564676d4ef0f90bd
DillonBarker <barker.dill98@gmail.com>
2020-09-08 15:15:06 +0100

    removed useless 0 - 0 statement, it is not needed

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -54,23 +54,28 @@ class Shop {
     }
   }
 
+  isNotBrieOrBackstagePass(i) {
+    if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+      this.isQualityAtMin(i)
+    } else {
+      this.isQualityAtMax(i) 
+    }
+  }
+
   // Their method (with my refactors)
   updateQuality() {
     for (var i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        this.isQualityAtMin(i)
-      } else {
-        this.isQualityAtMax(i)
-          this.BackstageSellInLessThan(i)
-      }
+      this.isNotBrieOrBackstagePass(i)
+
+      this.BackstageSellInLessThan(i)
+
       this.isNotSulfurasDecreaseSellBy(i)
+
       if (this.items[i].sellIn < 0) {
         if (this.items[i].name != 'Aged Brie') {
           if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
             this.isQualityAtMin(i)
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality;
-          }
+          } 
         } else {
           this.isQualityAtMax(i)
         }
