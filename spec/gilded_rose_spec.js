dbdb93c287879063aff0ecac9a99ad1a9d995e03
DillonBarker <barker.dill98@gmail.com>
2020-09-08 11:39:29 +0100

    written a test that shows the quality decreases by 2 once the sellIn has reached 0

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -4,7 +4,16 @@ describe("Gilded Rose", function() {
   it("should foo", function() {
     const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toEqual("fixme");
+    expect(items[0].name).toEqual("foo");
   });
+
+  it("should decrease quality twice as fast once sell by date has reached", function() {
+    const gildedRose = new Shop([ new Item("+5 Dexterity Vest", 1, 20) ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toEqual(19)
+    gildedRose.updateQuality();
+    expect(items[0].quality).toEqual(17)
+
+  })
 
 });
