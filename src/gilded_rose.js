0e5a42a50224b59f7a39df54e082c38182773f53
DillonBarker <barker.dill98@gmail.com>
2020-09-08 14:24:57 +0100

    adding a method that decreases the sell by of non-sulfuras items

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -17,9 +17,14 @@ class Shop {
     }
   };
 
-  isNotSulfurasDecreaseQuality(i){
+  isNotSulfurasDecreaseQuality(i) {
     if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
       this.items[i].quality = this.items[i].quality - 1;
+    }
+  };
+  isNotSulfurasDecreaseSellBy(i) {
+    if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+      this.items[i].sellIn = this.items[i].sellIn - 1;
     }
   };
 
@@ -42,9 +47,7 @@ class Shop {
           }
         
       }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
+      this.isNotSulfurasDecreaseSellBy(i)
       if (this.items[i].sellIn < 0) {
         if (this.items[i].name != 'Aged Brie') {
           if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
